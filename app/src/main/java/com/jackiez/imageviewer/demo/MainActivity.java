7999fe7f66e6632fed1999ffebb721ab386d517b
zhuangsigui <zhuangsigui@youmi.net>
2017-09-06 18:37:16 +0800

    up

--- app/src/main/java/com/jackiez/imageviewer/demo/MainActivity.java
+++ app/src/main/java/com/jackiez/imageviewer/demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.jackiez.imageviewer.demo;
 import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import java.io.IOException;
 
@@ -12,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TestView tv = (TestView) findViewById(R.id.v_pic);
+        final TestView tv = (TestView) findViewById(R.id.v_pic);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.mockClick();
+            }
+        });
         try {
             tv.setImage(getAssets().open("111.jpg", AssetManager.ACCESS_STREAMING));
         } catch (IOException e) {
--- app/src/main/java/com/jackiez/imageviewer/demo/TestView.java
+++ app/src/main/java/com/jackiez/imageviewer/demo/TestView.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
  */
 
 public class TestView extends View {
+
     public TestView(Context context) {
         this(context, null);
     }
@@ -45,7 +46,7 @@ public class TestView extends View {
                 int focusX = (int) (detector.getFocusX() + 0.5);
                 int focusY = (int) (detector.getFocusY() + 0.5);
 
-
+                Log.d("TestView", "onScale.scaleFactory = " + detector.getScaleFactor());
                 return false;
             }
 
@@ -61,6 +62,20 @@ public class TestView extends View {
         mScaleDetector = new ScaleGestureDetector(getContext(), scaleListener);
     }
 
+    /**
+     * 自动判断以宽或高作为初始显示图片的标准
+     */
+    public static int LOAD_BOTH = 0;
+    /**
+     * 规定以高作为初始显示图片的标准
+     */
+    public static int LOAD_VERTICAL = 1;
+    /**
+     * 规定以宽作为初始显示图片的标准
+     */
+    public static int LOAD_HORIZONTAL = 2;
+
+    private int mLoadState = LOAD_HORIZONTAL;
     private BitmapRegionDecoder mDecoder;
     private ScaleGestureDetector mScaleDetector;
     private int srcImageHeight;
@@ -68,7 +83,10 @@ public class TestView extends View {
     // 定义当前像素尺寸下可见
     private int centerX;
     private int centerY;
-    private int mScale = 1;
+    private float mScale = 1;
+    private int offset = 0;
+
+
     /**
      * 屏幕图片可见区域，最大跟屏幕区域相同
      */
@@ -90,7 +108,7 @@ public class TestView extends View {
         if (mScreenRect == null
                 || mScreenRect.width() == 0
                 || mScreenRect.height() == 0) {
-            mNeedInit = true;
+             mNeedInit = true;
             return;
         }
 
@@ -125,16 +143,25 @@ public class TestView extends View {
         // 获取显示的宽高比例
         float imgRate = (float) tmpImgWidth / tmpImgHeight;
         float screenRate = (float) screenWidth / screenHeight;
-        if (imgRate > screenRate) {
-            // 以宽作为标准，高进行变动
-            int realH = (int) (screenWidth / imgRate + 0.5f);
-            mVisibleRect.top = (screenHeight - realH) >> 1;
-            mVisibleRect.bottom = mVisibleRect.top + realH;
-        } else {
+        if ((mLoadState == LOAD_VERTICAL) ||
+                (mLoadState == LOAD_BOTH && imgRate > screenRate)) {
             // 以高作为标准，宽进行变动
             int realW = (int) (screenHeight * imgRate + 0.5f);
+            if (realW > screenWidth) {
+                mPicRect.right = (int) (srcImageHeight * screenRate + 0.5f);
+                realW = screenWidth;
+            }
             mVisibleRect.left = (screenWidth - realW) >> 1;
             mVisibleRect.right = mVisibleRect.left + realW;
+        } else {
+            // 默认，以宽作为标准，高进行变动
+            int realH = (int) (screenWidth / imgRate + 0.5f);
+            if (realH > screenHeight) {
+                mPicRect.bottom = (int) (srcImageWidth / screenRate + 0.5f);
+                realH = screenHeight;
+            }
+            mVisibleRect.top = (screenHeight - realH) >> 1;
+            mVisibleRect.bottom = mVisibleRect.top + realH;
         }
 
         // 初始化选择最适合的缩放比例
@@ -156,7 +183,6 @@ public class TestView extends View {
             Log.d("TestView", "startStcale = " + startScale);
             mOptions.inSampleSize = startScale;
         }
-
 
 
         Log.d("TestView", "pic.l = " + mPicRect.left + ", t = " + mPicRect.top + ", r = " + mPicRect.right + ", b = " + mPicRect.bottom);
@@ -190,7 +216,66 @@ public class TestView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return mScaleDetector.onTouchEvent(event);
+        mScaleDetector.onTouchEvent(event);
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Log.w("TestView", "点击位置坐标：(" + event.getX() + ", " + event.getY() + ")");
+            mDownX = (int) (event.getX() + 0.5f);
+            mDownY = (int) (event.getY() + 0.5f);
+        }
+        return super.onTouchEvent(event);
+    }
+
+    int mScrollX = 0;
+    int mScrollY = 0;
+    int mDownX;
+    int mDownY;
+
+    /**
+     * 模拟放大操作处理
+     */
+    public void mockClick() {
+        // 定义放大点位置 (1/2 * vw, 1/3 * vh)
+        // scrollX, scrollY
+        Log.d("TestView", "mockClick.call()");
+//        int scaleX = mVisibleRect.width() >> 1;
+//        int scaleY = mVisibleRect.height() / 3;
+        int scaleX = mDownX;
+        int scaleY = mDownY;
+
+        int realScaleX = (int) (scaleX / mScale + 0.5f);
+        int realScaleY = (int) (scaleY / mScale + 0.5f);
+
+        if (mScale == 1) {
+            // scale > 1 表示放大，即是单位可见区域显示像素缩小， scale < 1 则是缩小
+            // 此处表示放大两倍
+            mScale = 2;
+        } else {
+            mScale = 1;
+        }
+        int realImgWidth = (int) (srcImageWidth / mScale + 0.5f);
+        int realImgHeight = realImgWidth * getMeasuredHeight() / getMeasuredWidth();
+        // 计算中心点坐标的实际位置
+        int relativeX = (int) (realImgWidth * ((float) scaleX / mVisibleRect.width()) + 0.5f);
+        int relativeY = (int) (realImgHeight * ((float) scaleY / mVisibleRect.height()) + 0.5f);
+        Log.d("TestView", "mockClick.call() rx = " + relativeX + ", ry = " + relativeY + ", riw = " +realImgWidth + ", rih = " + realImgHeight
+        + ", realScaleX = " + realScaleX + ", realScaleY = " + realScaleY);
+        if (relativeX > realScaleX + mScrollX) {
+            mPicRect.left = 0;
+            mPicRect.right = realImgWidth;
+        } else {
+            mPicRect.left = realScaleX + mScrollX - relativeX;
+            mPicRect.right = mPicRect.left + realImgWidth;
+        }
+
+        if (relativeY > realScaleY + mScrollY) {
+            mPicRect.top = 0;
+            mPicRect.bottom = realImgHeight;
+        } else {
+            mPicRect.top = relativeY + mScrollY - relativeY;
+            mPicRect.bottom = mPicRect.top + realImgHeight;
+        }
+        invalidate();
+
     }
 
     @Override
@@ -223,7 +308,7 @@ public class TestView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        Log.d("TestView", "onDraw.isCall = " + mPicRect.left + ", " + mPicRect.top + ", " + mPicRect.right + ", " + mPicRect.bottom);
         // 进行当前图片区域计算
         // 计算切割实际图形区域大小，再实际绘制到屏幕上
         Bitmap bp = mDecoder.decodeRegion(mPicRect, mOptions);
